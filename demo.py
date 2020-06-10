2ab74eea1f5e20cd8fd2baaea2ec5820fa95d75b
Jordan Martel <jmartel@iu.edu>
2020-06-10 13:55:47 -0400

    DOC: added importance function example to README

--- demo.py
+++ demo.py
@@ -6,19 +6,6 @@ from scipy.integrate import fixed_quad
 from functools import lru_cache
 #import dm as dm
 
-    # importance function(s)
-    I = {
-            'i1' : lambda x: x**3., 
-            'i2' : lambda x: x**(-1.5),
-            'i3' : lambda x: (6.*(x-.5)**2.+.5)**3.
-        }
-
-    # for each importance function, plot discrete messages of size 5, 20, 100 
-    for i in I:
-        M = []
-        for n in [5,20,100]:
-            M = M + [dm.Message(n,I[i])]
-        dm.mplot(M)
 
 # beta test
 N = 10
