13b11ab4852a5e915ee3a9a3a5e3d777bedc0d0a
Jordan Michael Martel <jordan@jordanmartel.com>
2020-08-16 12:31:40 -0400

    ENH: reorder, retitle, resize figures

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,10 @@
 from discrete import Message
 from matplotlib import pyplot as plt
 
-B = [2.,1.,0.,-.1,-.2]
+B = [-.2,-.1,0.,1.,2.,4.]
 while B:
 	n = len(B)
 	_b = B.pop()
 	m = Message(M=100,N=4,b=_b)
-	m.plot_msg("documentation/msg" + str(n) + ".pdf")
-	m.plot_err("documentation/err" + str(n) + ".pdf",2.,6.)
+	m.plot_msg("../latex/Model_Figures/msg" + str(n) + ".pdf",title=False)
+	m.plot_err("../latex/Model_Figures/err" + str(n) + ".pdf",2.,6.,title=False)
