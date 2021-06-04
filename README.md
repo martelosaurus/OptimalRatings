14b77c7a5aa8b2def45b791843805b9bb6c9d917
jordan <jordan@jordanmartel.com>
2021-06-04 12:22:33 -0400

    DOC: updated README

--- README.md
+++ README.md
@@ -16,3 +16,17 @@ for i in I:
 		M = M + [dm.Message(n,I[i])]
 	dm.mplot(M)
 ```
+
+Discrete messages
+```python
+from discrete import Message
+from matplotlib import pyplot as plt
+
+B = [-.2,-.1,0.,1.,2.,4.]
+while B:
+    n = len(B)
+    _b = B.pop()
+    m = Message(M=100,N=4,b=_b)
+    m.plot_msg("../new_tex/Model_Figures/msg" + str(n) + ".pdf",title=False)
+    m.plot_err("../new_tex/Model_Figures/err" + str(n) + ".pdf",2.,6.,title=False)
+```
--- sandbox.py
+++ sandbox.py
@@ -3,8 +3,8 @@ from matplotlib import pyplot as plt
 
 B = [-.2,-.1,0.,1.,2.,4.]
 while B:
-	n = len(B)
-	_b = B.pop()
-	m = Message(M=100,N=4,b=_b)
-	m.plot_msg("../new_tex/Model_Figures/msg" + str(n) + ".pdf",title=False)
-	m.plot_err("../new_tex/Model_Figures/err" + str(n) + ".pdf",2.,6.,title=False)
+    n = len(B)
+    _b = B.pop()
+    m = Message(M=100,N=4,b=_b)
+    m.plot_msg("../new_tex/Model_Figures/msg" + str(n) + ".pdf",title=False)
+    m.plot_err("../new_tex/Model_Figures/err" + str(n) + ".pdf",2.,6.,title=False)
