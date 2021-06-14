da51a76a665d4d714be1315bda0e2ed652352e40
jordan <jordan@jordanmartel.com>
2021-06-14 16:28:03 -0400

    DEP: removed cost functions for identity, discrete

---
+++ tmp.py
@@ -0,0 +1,14 @@
+from optrat import Message, mplot
+# importance function(s)
+I = {
+    'i1' : lambda x: x**3., 
+    'i2' : lambda x: x**(-1.5),
+    'i3' : lambda x: (6.*(x-.5)**2.+.5)**3.
+    }
+
+# for each importance function, plot discrete messages of size 5, 20, 100 
+for i in I:
+    M = []
+    for n in [5,20,100]:
+        M = [Message(n,I[i])]
+        mplot(M)
