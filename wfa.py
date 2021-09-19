a37a4ab318024863407027ade17830ce06522c07
jordan <jordan@jordanmartel.com>
2021-09-19 14:31:22 -0400

    ENH: wfa work

--- wfa.py
+++ wfa.py
@@ -1,6 +1,33 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy.optimize import root
+from numpy.linalg import norm
+from constant_f import Message
 
 # Data (scores 0-10, no scores received 10)
-x = [.05,.20,.23,.17,.14,.10,.06,.02,.02,.01]
-#x = np.array(x)
+m_wfa = np.array([.05,.20,.23,.17,.14,.10,.06,.02,.02,.01,.0])
+N = len(m_wfa)
+q = np.r_[0:11]
+
+# plot distribution
+plt.bar(q,100*m_wfa)
+plt.xlabel("Referee score (scale of 0 to 10)")
+yt = np.r_[0:30:5]
+plt.yticks(yt,[str(t) + "%" for t in yt])
+plt.title("Distribution of submission reviews for 2014\nWestern Finance Association Meetings")
+plt.savefig("wfa.pdf")
+
+# committee reviews
+m_wfa = np.hstack([0,m_wfa])
+M_wfa = m_wfa.cumsum()
+
+# calibrartion
+a0 = np.ones(N+1)
+def f(a):
+    """a is a list of coefficients"""
+    m = Message(N,lambda x: a.dot(np.cos(np.r_[0:(N+1)]*np.arccos(x))))
+    return m.M-M_wfa
+
+# find zero
+sol = root(f,a0)
