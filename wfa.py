8976fc7e13027efe75a44d435d0e937dc092844f
jordan <jordan@jordanmartel.com>
2021-09-27 09:32:55 -0400

    ENH: wfa

--- wfa.py
+++ wfa.py
@@ -6,7 +6,7 @@ from numpy.linalg import norm
 from constant_f import Message
 
 # Data (scores 0-10, no scores received 10)
-m_wfa = np.array([.05,.20,.23,.17,.14,.10,.06,.02,.02,.01,.0])
+m_wfa = np.array([.05,.20,.23,.17,.14,.10,.06,.02,.02,.009,.001])
 N = len(m_wfa)
 q = np.r_[0:11]
 
@@ -15,7 +15,8 @@ plt.bar(q,100*m_wfa)
 plt.xlabel("Referee score (scale of 0 to 10)")
 yt = np.r_[0:30:5]
 plt.yticks(yt,[str(t) + "%" for t in yt])
-plt.title("Distribution of submission reviews for 2014\nWestern Finance Association Meetings")
+plt.title("Distribution of submission reviews for 2014\n"
+    "Western Finance Association Meetings")
 plt.savefig("wfa.pdf")
 
 # committee reviews
@@ -23,10 +24,10 @@ m_wfa = np.hstack([0,m_wfa])
 M_wfa = m_wfa.cumsum()
 
 # calibrartion
-a0 = np.ones(N+1)
+a0 = np.hstack([1,np.zeros(N-1)])
 def f(a):
     """a is a list of coefficients"""
-    m = Message(N,lambda x: a.dot(np.cos(np.r_[0:(N+1)]*np.arccos(x))))
+    m = Message(N,lambda x: a.dot(np.cos(np.r_[0:N]*np.arccos(2*x-1))))
     return m.M-M_wfa
 
 # find zero
