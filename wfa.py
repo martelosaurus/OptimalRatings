8201d6c4d94aed8b62865db4b344e381e50c55d4
Jordan Martel <jmartel@iu.edu>
2021-10-14 19:44:05 -0400

    ENH: wfa

--- wfa.py
+++ wfa.py
@@ -3,14 +3,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import fixed_quad
 from scipy.optimize import root
-from scipy.special import erf
+from scipy.sparse import spdiags
 from numpy.linalg import norm
 
 # parameters
 s = 1. # shape parameters
 
 # Data (scores 0-10, no scores received 10)
-x_wfa = np.array([.05,.20,.23,.17,.14,.10,.06,.02,.02,.009,.001])
+x_wfa = np.array([.05,.20,.23,.17,.14,.10,.06,.02,.02,.01,.0])
 N = len(x_wfa)
 q = np.r_[0:11]
 
@@ -21,16 +21,18 @@ yt = np.r_[0:30:5]
 plt.yticks(yt,[str(t) + "%" for t in yt])
 plt.title("Distribution of submission reviews for 2014\n"
     "Western Finance Association Meetings")
+plt.show()
 plt.savefig("wfa.pdf")
 plt.close()
 
 # committee reviews
 x_wfa = np.hstack([0,x_wfa])
 X_wfa = x_wfa.cumsum()
-
-# calibrartion
-a0 = np.ones(N+1)/N
-xk = np.linspace(0,1,N+1)
-
-def _Q(i,j,a,b,A=.1,_n=11):
-    return fixed_quad(lambda q: (q**i)*np.exp(-A*(q-xk[j])**2),a,b,n=_n)[0]
+g_wfa_x = .5*(X_wfa[1:]+X_wfa[:-1])
+g_wfa_y = 
+plt.xlabel("$q$")
+plt.ylabel("$m(q)$")
+plt.plot(X_wfa,X_wfa)
+plt.plot(X_wfa,x_plt)
+plt.grid()
+plt.show()
