b5df565ebfd8275493e1659126ad2ab0dc8bd1d1
Jordan Martel <jmartel@iu.edu>
2021-10-28 16:44:49 -0400

    ENH: misc

--- wfa.py
+++ wfa.py
@@ -7,7 +7,8 @@ from scipy.sparse import spdiags
 from numpy.linalg import norm
 
 # Data (scores 0-10, no scores received 10)
-x_wfa = np.array([.05,.20,.23,.17,.14,.10,.06,.02,.02,.009,.001])
+x_wfa = np.array([.05,.20,.23,.17,.14,.10,.06,.02,.02,.01,.0])
+#x_wfa = np.array([.05,.20,.23,.17,.14,.10,.06,.02,.02,.005,.005])
 N = len(x_wfa)
 q = np.r_[0:11]
 
@@ -20,8 +21,8 @@ plt.xticks(xt,xt)
 plt.yticks(yt,[str(t) + "%" for t in yt])
 plt.title("Distribution of submission reviews for 2014\n"
     "Western Finance Association Meetings")
-plt.show()
-plt.savefig("wfa.pdf")
+#plt.show()
+plt.savefig("wfa_hist.pdf")
 plt.close()
 
 # committee reviews
@@ -31,11 +32,12 @@ X_wfa = x_wfa.cumsum()
 g_wfa_x = .5*(X_wfa[1:]+X_wfa[:-1])
 g_wfa_y = (1/(X_wfa[1:]-X_wfa[:-1]))**3
 
+# plot
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-ax1.plot((1+q)/N,q,"--k",linewidth=2)
-ax1.plot(X_wfa,q,linewidth=2)
-ax2.plot(g_wfa_x,np.log(g_wfa_y),linewidth=2,color="k")
+ax1.plot((1+q)/N,q,"--",color="tab:blue",linewidth=2)
+ax1.plot(X_wfa,q,"-",color="tab:blue",linewidth=2)
+ax2.plot(g_wfa_x,g_wfa_y,'.k')
 ax1.set_xlabel("Paper quality percentile")
 ax1.set_ylabel("Referee score")
 ax2.set_ylabel("Density")
@@ -45,6 +47,7 @@ ax1.set_xticks(xt)
 ax1.set_xticklabels([str(t)+"%" for t in np.r_[0:110:10]])
 ax1.set_yticks(yt)
 ax2.set_yticklabels(yt)
-plt.legend(["Straight talk","WFA reviews","Inferred $I(q)$"])
+ax1.legend(["Straight talk","WFA reviews"])
+ax2.legend(["Inferred importance"],loc="upper center")
 plt.title("WFA Program Committee Reviews")
-plt.show()
+plt.savefig("wfa_dist.pdf")
